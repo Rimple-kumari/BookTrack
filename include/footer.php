8b7c013da0e6ad481de15db5f4209b8a58170842
Rimple-kumari <133660882+Rimple-kumari@users.noreply.github.com>
2025-06-20 10:41:55 +0530

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    2024 NITM Online Library Management System |<a href="https://github.com/Rimple-kumari" target="_blank"  > Designed by :Rimple </a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
